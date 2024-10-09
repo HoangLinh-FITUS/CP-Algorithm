23c0ac57cfcd73b988df0e489b33cb23ea1621e7
HoangLinh-FITUS <thlinh22@clc.fitus.edu.vn>
2024-10-09 09:43:46 +0700

    add 1068.cpp

--- cses/Introductory-Problems/1068.cpp
+++ cses/Introductory-Problems/1068.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-    
+    long long n;
+    cin >> n;
+    while (n > 1) {
+        cout << n << " ";
+        if (n & 1) n = 3 * n + 1;
+        else n /= 2;
+    }
+    cout << n;
     return 0;
 }
