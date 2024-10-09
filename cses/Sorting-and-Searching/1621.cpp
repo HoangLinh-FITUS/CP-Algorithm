bfbcbb1613b2886a26e7fa2dde15756954490b6d
HoangLinh-FITUS <thlinh22@clc.fitus.edu.vn>
2024-10-09 19:18:14 +0700

    add 1621.cpp

--- cses/Sorting-and-Searching/1621.cpp
+++ cses/Sorting-and-Searching/1621.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
 int main() {
-    
+    set <int> s;
+    int n;
+    cin >> n;
+    while (n--) {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }   
+    cout << s.size();
     return 0;
 }
